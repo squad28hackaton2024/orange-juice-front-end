6dee279e6f1f14fcb7795c86145dd1bf7437410d
Gabriel <gabriel.mazer@outlook.com>
2024-01-30 22:16:47 -0300

    feat: modificação do js para abrir popup

--- src/pages/TelaDescobrir/js/janela.js
+++ src/pages/TelaDescobrir/js/janela.js
@@ -1,25 +1,33 @@
 // Código que abre a janela do projeto ao clicar na div
-        const pegadiv = document.querySelectorAll('.projeto');
-        console.log(pegadiv);
+const divprojeto = document.querySelectorAll('.projeto');
+
 
         var i;
 
-        for(i = 0; i<pegadiv.length; i++){
-            pegadiv[i].onclick = abrirpop;
+        for(i = 0; i<divprojeto.length; i++){
+            divprojeto[i].onclick = abrirpop;
         }
 
         function abrirpop(){
             let pop = document.getElementById('pop');
-            let imagempop = document.getElementById('imagempop');
-            let imagembloco = this.children[0];
-            let nome = this.children[1];
+            let imagempop = document.getElementById('imagempopProjeto');
+            let imagemProjeto = this.children[0];
+            let imagemperfilpop = document.getElementById('imagemperfilpop');
+            let imagemPerfilProjeto = this.children[1].children[0];
+            let nomeProjeto = this.children[1].children[1];
+            let nomepop = document.getElementById('nomepop');
+            let telaescura = document.getElementById('telaescura');
             pop.style.display = 'block';
-            imagempop.src = imagembloco.src;
-            pop.children[1].innerText = this.children[1].innerText;
+            imagemperfilpop.src = imagemPerfilProjeto.src;
+            imagempop.src = imagemProjeto.src;
+            nomepop.innerText = nomeProjeto.innerText;
+            telaescura.style.display = 'block';
 
         }
 
         function fecharpop(){
             let pop = document.getElementById('pop');
             pop.style.display = 'none';
+            let telaescura = document.getElementById('telaescura');
+            telaescura.style.display = 'none';
         }
